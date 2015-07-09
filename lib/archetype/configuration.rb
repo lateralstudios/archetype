a2d56f342c42ea4f0301c1a536595301b92b826b
Tom Beynon <tombeynon@gmail.com>
2015-07-09 21:21:11 +0100

    Added javascripts to configuration
    TODO this is incomplete

--- lib/archetype/configuration.rb
+++ lib/archetype/configuration.rb
@@ -1,11 +1,15 @@
 module Archetype
   class Configuration
     attr_accessor :site_name, :user_method, :authenticate_method
+    attr_accessor :javascripts
     
     def initialize
       @site_name = 'Archetype Admin'
       @user_method = :current_admin
       @authenticate_method = :authenticate_admin!
+
+      # TODO: This is incomplete, and is interface specific
+      @javascripts = %w(archetype/application)
     end
   end
 end
