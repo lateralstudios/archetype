96beb948ed1c5cc7aaf300b54e302ef496c8f8c6
Tom Beynon <tombeynon@gmail.com>
2015-07-19 22:45:40 +0100

    Datetime presenter

---
+++ lib/archetype/attributes/presenters/datetime_presenter.rb
@@ -0,0 +1,23 @@
+module Archetype
+  module Attributes
+    module Presenters
+      class DatetimePresenter < AttributePresenter
+        def short_format(object)
+          format(object, '%e %B %Y %H:%M')
+        end
+
+        def long_format(object)
+          format(object) do |value|
+            value.strftime("#{value.day.ordinalize} %B %Y %H:%M:%S")
+          end
+        end
+
+        def format(object, format=nil, &block)
+          return unless value = from(object)
+          format = yield(value) if block_given?
+          value.strftime(format)
+        end
+      end
+    end
+  end
+end
